4765100760254d45caff3ecdd322c5d58d95f562
minnczi <kmj1995kr@gmail.com>
2021-03-24 11:47:36 +0900

    minji boj 4796

--- week1_3_15/BOJ_14716_민지.py
+++ week1_3_15/BOJ_14716_민지.py
@@ -1,27 +1,31 @@
 import sys
 sys.stdin = open('14716_input.txt', 'r')
 
+drc = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]]
+
+def DFS(cr, cc):
+    visited[cr][cc] = 1
+    stack = [(cr, cc)]
+    while stack:
+        cr, cc = stack.pop()
+        for dr, dc in drc:
+            nr, nc = cr+dr, cc+dc
+            if 0 <= nr < N and 0 <= nc <= M:
+                if not visited[nr][nc]:
+                    visited[nr][nc] = 1
+                    if canvas[nr][nc] == 1:
+                        stack.append((nr, nc))
+                    
+
 N, M = map(int, input().split())
 canvas = [list(map(int, input().split())) for _ in range(N)]
 
-drc = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]]
-
+cnt = 0
 visited = [[0]*M for _ in range(N)]
 
-cnt = 0
-for i in range(1, N-1):
-    for j in range(1, M-1):
-        stack = []
+for i in range(N):
+    for j in range(M):
         if canvas[i][j] == 1 and not visited[i][j]:
-            stack.append([i, j])
-            visited[i][j] = 1
-            while stack:
-                cr, cc = stack.pop()
-                visited[cr][cc] = 1
-                for dr, dc in drc:
-                    nr, nc = cr+dr, cc+dc
-                    if canvas[nr][nc] == 1 and not visited[nr][nc]:
-                        stack.append([nr, nc])
-                        visited[nr][nc] = 1
+            DFS(i, j)
             cnt += 1
 print(cnt)
