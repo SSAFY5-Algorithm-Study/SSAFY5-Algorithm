ee1c93de70b41255881ecbb3721ac6ec0b308182
minnczi <kmj1995kr@gmail.com>
2021-03-27 21:47:56 +0900

    heap정렬 설명 추가

--- week2_3_23/BOJ_15903_민지.py
+++ week2_3_23/BOJ_15903_민지.py
@@ -3,6 +3,12 @@ import heapq as hq
 """
 접근법: 무조건 현재 상황에서 가장 작은 두 수를 더하는 것이
 최종 카드의 합을 적게 만드는 방법이다
+
+힙 정렬 활용: 파이썬의 heapq 라이브러리를 활용하면 쉽게 사용할 수 있다
+힙 정렬을 사용하면 전체 리스트가 정렬 되지는 않지만, max나 min값만 반복해서 사용해야 할 경우
+작은 시간 복잡도로 사용할 수 있는 정렬 방법
+https://leedakyeong.tistory.com/entry/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%ED%8C%8C%EC%9D%B4%EC%8D%AC%EC%9C%BC%EB%A1%9C-%ED%9E%99-%EC%A0%95%EB%A0%AC-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-heap-sort-in-python
+
 """
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
