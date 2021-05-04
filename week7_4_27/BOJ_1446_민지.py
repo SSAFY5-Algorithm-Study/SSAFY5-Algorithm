d935ee30f35e93daf8163ab5fa8b1f5d8bbfc1f6
minnczi <kmj1995kr@gmail.com>
2021-05-04 19:12:54 +0900

    minji 지름길 solved

--- week7_4_27/BOJ_1446_민지.py
+++ week7_4_27/BOJ_1446_민지.py
@@ -1,20 +1,37 @@
-# 동전 0
-# Ai는 Ai-1의 배수라는 조건이 있어서 DP를 쓰지 않아도 풀이 가능
+# BOJ 1446 지름길
+"""
+접근법
+1. 지름길을 시작 위치 기준으로 오름차순 정렬한다
+2. idx 칸까지 가기 위한 최소 거리를 저장한 배열 min_dist를 만든다
+3. idx를 한칸씩 이동하면서, 현재 min_dist에 저장되어있는 값과 이전칸에서 한칸 더 이동했을때 두가지 경우 중 최소 값을 구한다
+4. 현재 위치에서 출발할 수 있는 지름길이 있는지 검사한다
+5. 현재 위치에서 이동할 수 있는 지름길이 있다면 min_dist[end]에 현재 저장되어 있는 값과 비교하여 더 짧은 지름길로 이동한다
+6. 도착지에 도착할때까지 반복한다
+
+"""
+
 import sys
-sys.stdin = open('input.txt', 'r')
+sys.stdin = open('input.txt')
 
-N, K = map(int, input().split())
+N, D = map(int, input().split())
+shortcuts = [list(map(int, input().split())) for _ in range(N)]
+shortcuts.sort()
 
-coins = []
-for _ in range(N):
-    coins.append(int(input()))
+min_dist = [10001] * (D+1)
+min_dist[0] = 0
+# 현재 위치
+idx = 0
+# 현재 위치까지의 최소거리
+temp_min = 0
 
-cnt = 0
-while K > 0:
-    for i in range(len(coins)-1, -1, -1):
-        if K >= coins[i]:
-            cnt += 1
-            K -= coins[i]
-            break
+while idx <= D:
+    # print(idx, temp_min)
+    temp_min = min_dist[idx] = min(min_dist[idx], temp_min+1)
+    for shortcut in shortcuts:
+        s, e, d = shortcut
+        if s == idx and e <= D:
+            min_dist[e] = min(min_dist[e], temp_min+d)
 
-print(cnt)
+    idx += 1
+
+print(min_dist[D])
