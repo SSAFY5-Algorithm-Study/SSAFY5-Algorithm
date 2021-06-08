72bbc7ddb0d74812184e99b527d060b292bf08d5
minnczi <kmj1995kr@gmail.com>
2021-06-08 18:35:11 +0900

    2644 solved

--- week10_6_11/BOJ_2644_민지(해결중).java
+++ week10_6_11/BOJ_2644_민지(해결중).java
@@ -1,8 +1,9 @@
-package week10;
+package week10_6_11;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -17,7 +18,10 @@ public class BOJ_2644_Minji {
 		int e = sc.nextInt();
 		int M = sc.nextInt();
 		
-		ArrayList<Integer>[] AL = new ArrayList[N+1];
+		List<Integer>[] AL = new ArrayList[N+1];
+		for (int i=0; i<=N; i++) {
+			AL[i] = new ArrayList<Integer>();
+		}
 		
 		for (int i=0; i<M; i++) {
 			int x = sc.nextInt();
@@ -27,25 +31,38 @@ public class BOJ_2644_Minji {
 			AL[y].add(x);
 		}
 		
-		int result = calculateChonsu(s, e, N);
+		int result = calculateChonsu(s, e, N, AL);
+		System.out.println(result);
+		
 		
 
 	}
 	
-	private static int calculateChonsu(int A, int B, int N) {
+	private static int calculateChonsu(int s, int e, int N, List<Integer>[] AL) {
 		int[] visited = new int[N+1];
+		visited[s] = 1;
 		
 		Queue<int[]> queue = new LinkedList<int[]>();
-		queue.add(new int[] {A,0});
+		queue.add(new int[] {s,0});
 		
 		while(queue.isEmpty() == false) {
-
+			int[] next = queue.remove();
+			
+			if (next[0] == e) {
+				return next[1];
+			}
+			
+			for (int i: AL[next[0]]) {
+				if (visited[i] == 0) {
+					queue.add(new int[] {i, next[1]+1});
+					visited[i] = 1;
+				}
+			}
 		}
+		return -1;
+		
 //		int[][] hello = new int[5][5];
 //		ArrayList<String>[] yeguk = new ArrayList[5];
-		
-		
-		return B;
 		
 	}
 
