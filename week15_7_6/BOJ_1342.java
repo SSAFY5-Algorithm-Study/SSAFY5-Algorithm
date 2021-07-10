f793b299ecaea7ea428ef31497b86fd7ac2d8e78
minnczi <kmj1995kr@gmail.com>
2021-07-11 00:10:54 +0900

    BOJ 1342 solution added in java

---
+++ week15_7_6/BOJ_1342.java
@@ -0,0 +1,53 @@
+package week15_7_6;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class BOJ_1342 {
+	private static Set<String> wordSet = new HashSet<String>();
+	private static int[] visited;
+	private static String raw = "";
+	
+	public static void main(String[] args) {
+
+		
+		// Input 받기
+		try {
+			Scanner sc = new Scanner(System.in);
+			raw = sc.next();
+			sc.close();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		visited = new int[raw.length()];
+		for (int i = 0; i < visited.length; i++) {
+			visited[i] = 0;
+		}
+		
+		for (int i = 0; i < visited.length; i++) {
+			visited[i] = 1;
+			isLuckyWord(1, raw.charAt(i), String.valueOf(raw.charAt(i)));
+			visited[i] = 0;
+		};
+		System.out.println(wordSet.size());
+	}
+	
+	private static void isLuckyWord(int level, char prev, String word) {
+		if (level == visited.length) {
+			wordSet.add(word);
+			return;
+		}
+		
+		for (int i = 0; i < visited.length; i++) {
+			if (visited[i] == 0 & raw.charAt(i) != prev) {
+				visited[i] = 1;
+				isLuckyWord(level+1, raw.charAt(i), word + String.valueOf(raw.charAt(i)));
+				visited[i] = 0;
+			}
+		}
+	};
+}
--- week15_7_6/BOJ_1342_민지.py
+++ week15_7_6/BOJ_1342_민지.py
@@ -13,15 +13,12 @@ def is_lucky_word(level, prev, word):
 raw = input()
 word_set = set()
 N = len(raw)
-cnt = 0
 visited = [0] * 10
 
-if N == len(set(raw)):
-    print(factorial(N))
-else:        
-    for i in range(N):
-        visited[i] = 1
-        is_lucky_word(1, raw[i], raw[i])
-        visited[i] = 0
+       
+for i in range(N):
+    visited[i] = 1
+    is_lucky_word(1, raw[i], raw[i])
+    visited[i] = 0
 
-    print(len(word_set))
+print(len(word_set))
