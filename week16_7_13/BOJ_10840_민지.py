7ce4c0ede4c8e298d19f7b605eb0460e21638bbb
minnczi <kmj1995kr@gmail.com>
2021-07-15 23:46:11 +0900

    boj10840 tried

---
+++ week16_7_13/BOJ_10840_민지.py
@@ -0,0 +1,21 @@
+word1 = input()
+word2 = input()
+
+def initialize_dict(word, n):
+    word_dict = {}
+    for i in range(n):
+        word_dict[i] = word_dict.get(word[i], 0) + 1
+    return word_dict
+
+def check_interval_component(n):
+    word1_dict = initialize_dict(word1, n)
+    word2_dict = initialize_dict(word2, n)
+
+    for i in range
+
+
+
+length = min(len(word1), len(word2))
+
+for i in range(length, 0, -1):
+    check_interval_component(i)
