0e1a66e5bbadc1b0efb194f751e8a37cdf6bb1e1
minnczi <kmj1995kr@gmail.com>
2021-07-26 20:51:43 +0900

    Minji BOJ_9655 solved in Java

---
+++ week17_7_20/BOJ_9655_민지.java
@@ -0,0 +1,22 @@
+package week17_7_20;
+
+import java.util.Scanner;
+
+public class BOJ_9655 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        String[] winner = new String[N+1];
+        winner[0] = "CY";
+        winner[1] = "SK";
+
+        if (N >= 2) {
+            for (int i=2; i<=N; i++) {
+                winner[i] = winner[i-2];
+            }
+        }
+        System.out.println(winner[N]);
+    }
+}
