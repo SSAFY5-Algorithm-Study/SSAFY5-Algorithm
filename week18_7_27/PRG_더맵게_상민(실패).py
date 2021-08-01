3acee5dabddae1454d337ba9231d1c9ce6037508
ssmin0606 <64825713+ssmin0606@users.noreply.github.com>
2021-08-01 22:31:36 +0900

    Create PRG_더맵게_상민(실패).py

---
+++ week18_7_27/PRG_더맵게_상민(실패).py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    while True:
+        min_value = heapq.heappop(scoville)
+        if min_value >= K:
+            return answer
+        if not len(scoville):
+            return -1
+        min_second_value = heapq.heappop(scoville) * 2
+        new_value = min_value + min_second_value
+        heapq.heappush(scoville, new_value)
+        answer += 1
+    return answer
