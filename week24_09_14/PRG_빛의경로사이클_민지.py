a28bd879f8777d9065e4bc100f18c0125f1055ae
minnczi <kmj1995kr@gmail.com>
2021-09-22 10:37:18 +0900

    MK: PRG_빛의경로사이클 - solved

--- week24_09_14/PRG_빛의경로사이클_민지.py
+++ week24_09_14/PRG_빛의경로사이클_민지.py
@@ -1,5 +1,21 @@
 """
-코드가 너무 더러움.. 수정하겠슴다..
+변수 설명
+grid: 타일을 한 글자씩 저장해 놓은 2차원 배열
+[
+    ['S', 'L'],
+    ['L', 'R']
+]
+visited: 각 타일 별로 상/하/좌/우 네방향에서 빛이 통과한 적이 있는지 검사하는 3차원 배열
+[
+    [[0,0,0,0], [0,0,0,0]],
+    [[0,0,0,0], [0,0,0,0]]
+]
+cr, cc: 현재 위치 (row, column)
+cd: 현재 타일로 올때 빛이 날아온 방향
+dist: 현재 사이클의 총 길이
+
+LR: length row - 총 행의 갯수
+LC: length col - 총 열의 갯수
 """
 # 상하좌우 순서대로 해당 방향에서 빛이 날아왔을때,
 # 반사 빛이 어디로 이동하는지를 저장해 놓은 dict
@@ -11,6 +27,7 @@ direction = {
     'R': [[0,-1,3], [0,1,2], [1,0,0], [-1,0,1]]
 }
 
+
 def find_cycle(cr, cc, cd, grid):
     dist = 0
     while visited[cr][cc][cd] == 0:
@@ -20,28 +37,29 @@ def find_cycle(cr, cc, cd, grid):
         
         # 그 다음 이동할 칸
         dr, dc, cd = direction[node][cd]
-        cr, cc = (cr+dr) % len(grid), (cc+dc) % N
-        if cr < 0:
-            cr = N-1
-        if cc < 0:
-            cc = N-1
+        cr, cc = (cr+dr) % LR, (cc+dc) % LC
     return dist
 
 
 def solution(grid):
-    global visited, N
+    global visited, N, LR, LC
+
+    LR = len(grid)
+    LC = len(grid[0])
     
-    for i in range(len(grid)):
+    # 사용하기 편하게 grid의 각 글자를 한글자씩 떼어주기
+    for i in range(LR):
         grid[i] = list(grid[i])
-    N = len(grid[i])
     
-    visited = [[[0]*4 for j in range(len(grid[i]))] for i in range(len(grid))]
+    # 3차원 visit 배열 생성
+    visited = [[[0]*4 for _ in range(LC)] for _ in range(LR)]
     
     answer = []
-    for i in range(len(grid)):
-        for j in range(N):
+    # 아직까지 방문하지 않은 노드+방향 조합이 있다면, 그 곳을 시작점으로 해서 탐색 시작
+    for i in range(LR):
+        for j in range(LC):
             for k in range(4):
                 if not visited[i][j][k]:
                     answer.append(find_cycle(i, j, k, grid))
-
+                    
     return sorted(answer)
