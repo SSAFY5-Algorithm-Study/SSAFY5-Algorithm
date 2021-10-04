6c22d991e6dcd34fc02b90508db9f107094d3ba9
minnczi <kmj1995kr@gmail.com>
2021-10-04 20:55:52 +0900

    MK: BOJ_7490 - solved

---
+++ week26_09_28/BOJ_7490_민지.py
@@ -0,0 +1,48 @@
+T = int(input())
+
+concats = [
+    lambda x, y: (str(x) + ' ' + str(y)),
+    lambda x, y: (str(x) + '+' + str(y)),
+    lambda x, y: (str(x) + '-' + str(y))
+]
+
+ops_dict = {
+    '+': lambda x, y: (x + y),
+    '-': lambda x, y: (x - y)
+    }
+
+def evaluate(eq):
+    eq = eq.replace(' ', '')
+    result = 0
+    num = ''
+    sign = '+'
+
+    for i in eq:
+        if i.isdigit():
+            num += i
+        else:
+            result = ops_dict[sign](result, int(num))
+            num = ''
+            sign = i
+    
+    result = ops_dict[sign](result, int(num))
+    return result
+    
+
+def make_zero(eq, num):
+    if num == N+1:
+        if not evaluate(eq):
+            print(eq)
+        return
+
+    for concat in concats:
+        new_eq = concat(eq, num)
+        make_zero(new_eq, num+1)
+
+for _ in range(T):
+    global N
+    N = int(input())
+    # 1~N 까지의 리스트 생성
+    nums = list(range(1, N+1))
+    make_zero('1', 2)
+    print("")
